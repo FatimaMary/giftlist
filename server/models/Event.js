c75526765604032c86dd3fdb78ea2bc17cd691fc
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-02 20:08:21 +0530

    User data Model created

---
+++ server/models/Event.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+    },
+    birthDay: {
+        type: Date
+    },
+});
+
+const Users = mongoose.model("GiftlistUsers", userSchema);
+
+export default Users;
