cc4851dda4270d10b38a0ee4710f2c4b4824df4e
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-06 11:23:20 +0530

    Update index.jsx

--- client/src/scenes/budget/index.jsx
+++ client/src/scenes/budget/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { 
+  Box, 
+  Button, 
+  TextField, 
+  Typography,
+} from '@mui/material';
 
 function Budget() {
-  return (
-    <div>budget</div>
-  )
+  return <Box m='1.5rem 2.5rem'>
+    <Typography variant='h4'>Gift exchange rules</Typography>
+    <Box>
+      <Typography>Spending limit per gift</Typography>
+      <TextField /> 
+    </Box>
+    <Box>
+      <Typography>Other details(Optional)</Typography>
+      <TextField />
+    </Box>
+    <Box>
+      <Button>
+        Submit
+      </Button>
+    </Box>
+  </Box>
 }
 
 export default Budget
