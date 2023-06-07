141f55b811f392636f65dc66c11070c015f987bd
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-07 10:56:43 +0530

    Deleted sidebar files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { themeSettings } from "theme";
 import Signup from './scenes/Signup';
 import Login from './scenes/login';
 import EventCreation from './scenes/eventcreation';
 import Signup1 from './scenes/Signup1';
 import Budget from './scenes/budget';
 import GiftExchange from './scenes/giftExchange';
-import Layout from './scenes/layout';
 
 
 function App() {
@@ -16,7 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout/>}>
+          <Route >
             <Route path='/' element={<Signup/>} />
             <Route path='/signup1' element={<Signup1/>} />
             <Route path='/login' element={<Login/>} />
