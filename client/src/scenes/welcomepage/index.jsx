0608a749124c033740615cbc8435dd26539d998f
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-08 15:15:48 +0530

    Update index.jsx

--- client/src/scenes/welcomepage/index.jsx
+++ client/src/scenes/welcomepage/index.jsx
@@ -26,9 +26,35 @@ function WelcomePage() {
             }}
             className='boxcomponent'
         >
-            <Typography variant='h2'>Gifting made easy</Typography>
-            <Typography variant='h6'>GiftList is the easiest way to exchange gifts with friends and family for birthdays, holidays, and more!</Typography>
-            <Button variant='contained' onClick={handleClick}>Create a giftlist</Button>
+            <Typography 
+                variant='h2'
+            >
+                Gifting made easy
+            </Typography>
+            <Typography 
+                variant='h6'
+                sx={{
+                    fontSize: '1.5rem',
+                    width: '600px',
+                    opacity: '0.5'
+                }} 
+            >
+                GiftList is the easiest way to exchange gifts with friends and family for birthdays, holidays, and more!
+            </Typography>
+            <Button 
+                variant='contained' 
+                onClick={handleClick}
+                sx={{
+                    background: '#2B2A4C',
+                    padding: '10px 15px',
+                    borderRadius: '10px',
+                    textTransform: 'inherit',
+                    fontSize: '1rem',
+                    letterSpacing: '1px',
+                }}
+            >
+                Create a gift list
+            </Button>
         </Box>
     </Box>
   )
