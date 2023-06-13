24038fa82bbf97e69589da11d391c8c97cb3acfe
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-13 12:11:43 +0530

    Update index.jsx

--- client/src/scenes/eventview/index.jsx
+++ client/src/scenes/eventview/index.jsx
@@ -7,6 +7,8 @@ import {
  import Santa from '../giftExchange/santa1.png';
  import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import EditCalendarOutlinedIcon from '@mui/icons-material/EditCalendarOutlined';
 
 function EventView() {
     function stringToColor(string) {
@@ -31,10 +33,10 @@ function EventView() {
 
     function stringAvatar(name) {
         return {
-          sx: {
-            bgcolor: stringToColor(name),
-          },
-          children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+        //   sx: {
+        //     background: '#50bcd9',
+        //   },
+          children: `${name.split(' ')[0][0]}`,
         };
       }
   return <Box 
@@ -53,29 +55,210 @@ function EventView() {
                 <img 
                     src={Santa} 
                     alt='santa image' 
-                    height={250} 
+                    height={200} 
                     width={250}
                     className='starimage'
                 />
             </Box>
             <Box sx={{
-                background: '#fff',
-                border: '1px solid #e8ecf1',
-                borderRadius: '10px',
-                maxWidth: '700px'
+                width: '100%',
+                margin: '15px',
             }}>
+                <Box 
+                  sx = {{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    marginBottom: '15px'
+                  }}
+                >
+                    <Typography
+                        variant='h2'
+                        sx={{
+                            fontSize: '28px',
+                            lineHeight: '34px',
+                            marginBottom: 0,
+                            maxWidth: '73%',
+                            fontWeight: 600,
+                            color: '#101a34',
+                        }}
+                    >
+                        Event name
+                    </Typography>
+                    <Box 
+                       sx={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent: 'flex-end'
+                       }}
+                    >
+                        <Button
+                            sx={{
+                                marginRight: '12px',
+                                padding: '8px 15px',
+                                fontWeight: 600,
+                                fontSize: '13px',
+                                lineHeight: '18px',
+                                borderRadius: '5px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                background: '#50bcd9',
+                                color: '#fff',
+                                border: '1px solid #50bcd9',
+                                textTransform: 'inherit'
+                            }}
+                        >
+                            <ShareOutlinedIcon 
+                                sx={{
+                                    width: '16px', 
+                                    height: '16px',
+                                    marginRight: '5px'
+                                }}
+                            />
+                            Invite
+                        </Button>
+                        <Button
+                             sx={{
+                                marginRight: '12px',
+                                padding: '8px 15px',
+                                fontWeight: 600,
+                                fontSize: '13px',
+                                lineHeight: '18px',
+                                borderRadius: '5px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                background: '#fafbfd',
+                                color: '#101a34',
+                                border: '1px solid #cad3dd',
+                                textTransform: 'inherit'
+                            }}
+                        >
+                            <EditCalendarOutlinedIcon 
+                                 sx={{
+                                    width: '16px', 
+                                    height: '16px', 
+                                    marginRight: '5px'
+                                }}
+                            />
+                            Edit
+                        </Button>
+                    </Box>
+                </Box>
                 <Box 
                     sx={{
                         display: 'flex',
-                        justifyContent: 'space-around',
+                        justifyContent: 'felx-start',
+                        alignItems: 'center',
+                        marginBottom: '22px',
+                        // gap: '25px',
+
                     }}
                 >
-                    <Typography>Exchange gifts by: Date</Typography>
-                    <Typography>RSVP by: Date</Typography>
-                    <Typography>Gift budget: Amount </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '13px',
+                            lineHeight: '16px',
+                            color: '#5e6577',
+                            paddingRight: '15px',
+                            marginRight: '15px',
+                            borderRight: '1px solid #cad3dd',
+                        }}
+                    >
+                        Exchange gifts by: <span style={{fontWeight: 600, color: '#101a34'}}>Date</span>
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '13px',
+                            lineHeight: '16px',
+                            color: '#5e6577',
+                            paddingRight: '15px',
+                            marginRight: '15px',
+                            borderRight: '1px solid #cad3dd',
+                        }}
+                    >
+                        RSVP by: <span style={{fontWeight: 600, color: '#101a34'}}>Date</span>
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '13px',
+                            lineHeight: '16px',
+                            color: '#5e6577',
+                            paddingRight: '15px',
+                            marginRight: '15px',
+                            // borderRight: '1px solid #cad3dd',
+                        }}
+                    >
+                        Gift budget: <span style={{fontWeight: 600, color: '#101a34'}}>Amount</span> 
+                    </Typography>
                 </Box>
-                <Box>
-                    <Avatar {...stringAvatar('Fatima Mary')} />
+                <Box 
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                    marginBottom: '10px',
+                    width: '100%',
+                  }}
+                >
+                    <Avatar 
+                        {...stringAvatar('Organizer name')} 
+                        sx={{
+                            width: '40px',
+                            height: '40px',
+                            background: '#50bcd9',
+                            fontSize: '13px',
+                        }}
+                    />
+                    <Box 
+                      sx={{
+                        flexBasis: 'calc(100% - 48px)',
+                        maxWidth: 'calc(100% - 48px)',
+                        paddingLeft: '12px'
+                      }}
+                    >
+                        <Typography
+                            sx={{
+                                fontWeight: 400,
+                                fontSize: '13px',
+                                lineHeight: '16px',
+                                color: '#818694',
+                                marginBottom: 0,
+                            }}
+                        >
+                            Organizer
+                        </Typography>
+                        <Typography
+                            variant='h4'
+                            sx={{
+                                fontWeight: 600,
+                                fontSize: '17px',
+                                lineHeight: '22px',
+                                color: '#101a34',
+                                marginBottom: 0,
+                            }}
+                        >
+                            Organizer name
+                        </Typography>
+                    </Box>
+                </Box>
+                <Box 
+                  sx={{
+                    fontSize: '15px',
+                    lineHeight: '140%',
+                    color: '#5e6577',
+                    paddingBottom: '17px',
+                    borderBottom: '1px solid #cad3dd',
+                  }}
+                >
+                    <Typography
+                        sx={{
+                            height:'20px',
+                            overflow: 'hidden',
+                            maxWidth: '500px',
+                            wordBreak: 'break-word',
+                            margin: '0px'
+                        }}
+                    >Join our son's birthday celebration</Typography>
                 </Box>
             </Box>
         </Box>
