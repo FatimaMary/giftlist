44218ac898ed73f3687bfe2482992e348c4b16ac
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-15 16:49:50 +0530

    Create index.jsx

---
+++ client/src/scenes/participants/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { 
+    Box, 
+    Typography, 
+} from '@mui/material'
+
+function Participants() {
+  return (
+    <div>Participants</div>
+  )
+}
+
+export default Participants
