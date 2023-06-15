78524db7ed5e2c089bb9491171c7e84db549c7ca
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-15 19:53:33 +0530

    Participants page updated

--- client/src/scenes/participants/index.jsx
+++ client/src/scenes/participants/index.jsx
@@ -2,12 +2,49 @@ import React from 'react';
 import { 
     Box, 
     Typography, 
-} from '@mui/material'
+    Button,
+} from '@mui/material';
+import Tick from './tickic.svg';
 
 function Participants() {
-  return (
-    <div>Participants</div>
-  )
+  return <Box sx={{
+    padding: '15px 0',
+    display: 'flex',
+    flexWrap: 'wrap',
+  }}>
+    <Box sx={{width: '272px'}}>
+        <Box
+          sx={{
+            padding: '24px 16px',
+            background: '#fff',
+            border: '1px solid #e8ecf1',
+            borderRadius: '10px',
+            color: '#101a34',
+            lineHeight: '25px',
+          }}
+        >
+            <Box 
+                sx = {{
+                    display: 'flex', 
+                    alignItems: 'center'
+                }}
+            >
+                <img 
+                    src={Tick} 
+                    style={{
+                        width: '18px', 
+                        height: '18px', 
+                        marginRight: '6px'
+                    }} 
+                />
+                <Typography
+                    
+                >Participating()</Typography>
+            </Box>
+        </Box>
+    </Box>
+    <Box></Box>
+  </Box>
 }
 
 export default Participants
