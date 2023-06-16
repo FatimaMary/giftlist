2eadca947ea664f1f892ec9ee5373020cd0d86f3
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-16 18:52:41 +0530

    Update invite.jsx

--- client/src/scenes/success/invite.jsx
+++ client/src/scenes/success/invite.jsx
@@ -6,9 +6,41 @@ import {
 } from '@mui/material';
 
 function invite() {
-  return (
-    <div>invite</div>
-  )
+  return <Box
+    sx={{
+        margin: '0 361px',
+        display: 'flex',
+    }}
+  >
+    <Box 
+        sx={{
+            background: '#fff',
+            border: '1px solid #cad3dd',
+            boxShadow: '5px 5px 25px -5px rgba(32, 32, 36, .1)',
+            borderRadius: '10px',
+            padding: '24px 20px',
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+        }}
+    >
+        <Box 
+            sx={{
+                padding:'0 0 16px',
+                borderBottom: '1px solid #e8ecf1',
+                flexWrap: 'wrap',
+                display: 'flex',
+                flexShrink: 0,
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}
+        >
+            <Typography>Add email address</Typography>
+            <Button>X</Button>
+        </Box>
+        <Box></Box>
+    </Box>
+  </Box>
 }
 
 export default invite
