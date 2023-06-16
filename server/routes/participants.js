acef6ed47561b056dc4bc84ccacaea4d93ae0e42
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-16 17:09:28 +0530

    Update participants.js

--- server/routes/participants.js
+++ server/routes/participants.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import { 
+    postParticipant,
+    getAllParticipants,
+} from '../controllers/participants.js';
 
 const router = express.Router();
+
+router.post("/post", postParticipant);
+router.get("/all", getAllParticipants);
 
 export default router;
