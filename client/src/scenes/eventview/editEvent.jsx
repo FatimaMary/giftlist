baf75fd5823c039861c131d4ba32dfb1f1a9d947
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-17 16:55:37 +0530

    Update editEvent.jsx

--- client/src/scenes/eventview/editEvent.jsx
+++ client/src/scenes/eventview/editEvent.jsx
@@ -4,13 +4,36 @@ import {
     TextField,
     Button,
     Dialog,
-    DialogContent
+    DialogContent,
+    DialogTitle
 } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function EditEvent() {
-  return (
-    <div>EditEvent</div>
-  )
+  return <Dialog
+    sx={{
+        margin: '0 361px',
+        display: 'flex',
+    }}
+  >
+    <DialogContent
+        sx={{
+            background: '#fff',
+            boxShadow: '5px 5px 25px -5px rgba(32, 32, 36, .1)',
+            borderRadius: '10px',
+            padding: '24px 20px',
+            display: 'flex',
+            flexDirection: 'column',
+            width: '500px',
+            justifyContent: 'center',
+        }}
+    >
+        <DialogTitle>
+           <Typography>Edit event</Typography>
+           <Button><DeleteIcon/>Delete</Button>
+        </DialogTitle>
+    </DialogContent>
+  </Dialog>
 }
 
 export default EditEvent
