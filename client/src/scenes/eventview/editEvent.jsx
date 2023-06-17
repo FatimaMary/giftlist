11d0375823156d16074b23e180ca931fc897ca64
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-17 16:29:02 +0530

    Create editEvent.jsx

---
+++ client/src/scenes/eventview/editEvent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Typography, 
+    TextField,
+    Button,
+    Dialog,
+    DialogContent
+} from '@mui/material';
+
+function EditEvent() {
+  return (
+    <div>EditEvent</div>
+  )
+}
+
+export default EditEvent
