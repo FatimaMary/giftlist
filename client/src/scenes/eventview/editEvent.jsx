1ad73debf67d614c006c597c68094951d66c9091
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-19 16:03:54 +0530

    Update editEvent.jsx

--- client/src/scenes/eventview/editEvent.jsx
+++ client/src/scenes/eventview/editEvent.jsx
@@ -8,7 +8,8 @@ import {
     DialogTitle,
     Box,
     InputLabel,
-    createTheme
+    createTheme,
+    ThemeProvider
 } from '@mui/material';
 import Delete from './removeic.svg';
 
@@ -127,25 +128,30 @@ function EditEvent({ open, onClose, editPage }) {
                             fontSize: '13px',
                             lineHeight: '18px',
                             color: '#101a34',
-                            marginBottom: '5px',
+                            // marginBottom: '5px',
                             wordBreak: 'break-all'
                         }}
                     >
                         Name
                     </InputLabel>
-                    <TextField sx={{
-                        background: '#fff',
-                        borderRadius: '7px',
-                        width: '100%',
-                        padding: '8px 15px',
-                        fontWeight: 400,
-                        fontSize: '16px',
-                        color: '#101a34',
-                        '& .MuiOutlinedInput-root': {
-                            height: '44px',
-                        },
-                        marginLeft: '-15px'
-                    }} />
+                    <ThemeProvider theme={theme}>
+                        <TextField 
+                            sx={{
+                                background: '#fff',
+                                borderRadius: '7px',
+                                width: '100%',
+                                padding: '8px 15px',
+                                fontWeight: 400,
+                                fontSize: '16px',
+                                color: '#101a34',
+                                '& .MuiOutlinedInput-root': {
+                                    height: '44px',
+                                },
+                                marginLeft: '-15px'
+                            }}
+                            type='text'
+                        />
+                    </ThemeProvider>
                 </Box>
                 <Box marginBottom='20px'>
                     <Box 
@@ -153,13 +159,166 @@ function EditEvent({ open, onClose, editPage }) {
                         margin: '0 -4px',
                         display: 'flex',
                         flexWrap: 'wrap',
+                        justifyContent: 'space-around',
+                        width: '100%'
                       }}
                     >
-                        <Box>
-                            <InputLabel>Gift exchange date:</InputLabel>
-                            <TextField />
+                        <Box 
+                            p='0 4px'
+                            sx={{
+                                width: '40%'
+                            }}
+                        >
+                            <InputLabel 
+                                sx={{
+                                    fontWeight: 600,
+                                    fontSize:'13px',
+                                    lineHeight: '18px',
+                                    color: '#101a34',
+                                    marginBottom: '5px',
+                                    wordBreak: 'break-all',
+                                }}
+                            >
+                                Gift exchange date:
+                            </InputLabel>
+                            <ThemeProvider theme={theme} >
+                            <TextField 
+                                type='date'
+                                sx={{
+                                    '& .MuiOutlinedInput-root': {
+                                        height: '44px',
+                                    },
+                                    width: '100%'
+                                }}
+                            />
+                            </ThemeProvider>
+                        </Box>
+                        <Box 
+                             p='0 4px'
+                             sx={{
+                                 width: '40%'
+                             }}
+                        >
+                            <InputLabel 
+                                sx={{
+                                    fontWeight: 600,
+                                    fontSize:'13px',
+                                    lineHeight: '18px',
+                                    color: '#101a34',
+                                    marginBottom: '5px',
+                                    wordBreak: 'break-all',
+                                }}
+                            >
+                                RSVP date:
+                            </InputLabel>
+                            <ThemeProvider theme={theme} >
+                            <TextField 
+                                type='date'
+                                sx={{
+                                    '& .MuiOutlinedInput-root': {
+                                        height: '44px',
+                                    },
+                                    width: '100%'
+                                }}
+                            />
+                            </ThemeProvider>
                         </Box>
                     </Box>
+                    <Box>
+                        <Typography
+                            sx={{
+                                fontSize: '13px',
+                                lineHeight: '20px',
+                                color: '#5e6577',
+                                marginBottom: 0,
+                                marginTop: '6px',
+                            }}
+                        >Names will be drqwn the day after the RSVP deadline</Typography>
+                    </Box>
+                </Box>
+                <Box marginBottom='20px'>
+                <InputLabel 
+                        sx={{
+                            fontWeight: 600,
+                            fontSize: '13px',
+                            lineHeight: '18px',
+                            color: '#101a34',
+                            // marginBottom: '5px',
+                            wordBreak: 'break-all'
+                        }}
+                    >
+                        Spending limit per gift
+                    </InputLabel>
+                    <ThemeProvider theme={theme}>
+                        <TextField 
+                            sx={{
+                                background: '#fff',
+                                borderRadius: '7px',
+                                width: '100%',
+                                padding: '8px 15px',
+                                fontWeight: 400,
+                                fontSize: '16px',
+                                color: '#101a34',
+                                '& .MuiOutlinedInput-root': {
+                                    height: '44px',
+                                },
+                                marginLeft: '-15px'
+                            }}
+                            type='text'
+                        />
+                    </ThemeProvider>
+                </Box>
+                <Box marginBottom='20px'>
+                <InputLabel 
+                        sx={{
+                            fontWeight: 600,
+                            fontSize: '13px',
+                            lineHeight: '18px',
+                            color: '#101a34',
+                            // marginBottom: '5px',
+                            wordBreak: 'break-all'
+                        }}
+                    >
+                        Other details
+                    </InputLabel>
+                    <ThemeProvider theme={theme}>
+                        <TextField 
+                            sx={{
+                                background: '#fff',
+                                borderRadius: '7px',
+                                width: '100%',
+                                padding: '8px 15px',
+                                fontWeight: 400,
+                                fontSize: '16px',
+                                color: '#101a34',
+                                marginLeft: '-15px'
+                            }}
+                            type='text'
+                            multiline
+                            rows={4}
+                            placeholder='Enter Your text'
+                        />
+                    </ThemeProvider>
+                </Box>
+                <Box>
+                    <Button 
+                        sx={{
+                            background: '#50bcd9',
+                            borderRadius: '7px',
+                            fontSize: '15px',
+                            lineHeight: '22px',
+                            color: '#fff',
+                            padding: '14px 30px',
+                            border: '1px solid #50bcd9',
+                            fontWeight: 600,
+                            width: '100%',
+                            textTransform: 'none',
+                            '&: hover': {
+                                color: '#50bcd9',
+                                background: '#fff'
+                            }
+                        }}
+                    >Save changes</Button>
                 </Box>
             </form>
         </Box>
