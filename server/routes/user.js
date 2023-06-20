f2295d1354100daad0f230e93ebf340ea12940fe
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-20 17:20:42 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ import {
     postUser,
     updateUser,
     getUser,
+    getUserByEmail,
 } from '../controllers/user.js';
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/add", postUser);
 router.get("/all", getUser);
 router.put("/:userId", updateUser);
+router.get("/:email", getUserByEmail);
 
 export default router;
