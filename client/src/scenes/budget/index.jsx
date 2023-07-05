ed1396c62f2a83d4eee505a113067187a09e3734
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-05 15:26:37 +0530

    Update index.jsx

--- client/src/scenes/budget/index.jsx
+++ client/src/scenes/budget/index.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { 
-  Box, 
-  Button, 
-  TextField, 
-  ThemeProvider, 
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  ThemeProvider,
   Typography,
   createTheme,
-} from '@mui/material';
-import axios from 'axios';
-import { useSearchParams, useNavigate } from 'react-router-dom';
-import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
-import Gift from './gift.png';
-import Footer from '../../Components/Footer';
+} from "@mui/material";
+import axios from "axios";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
+import Gift from "./gift.png";
+import Footer from "../../Components/Footer";
 
 function Budget() {
   const [budget, setBudget] = useState();
@@ -26,14 +26,14 @@ function Budget() {
       MuiTextField: {
         styleOverrides: {
           root: {
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: '#cad3dd', 
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#cad3dd",
               },
             },
-            '& .MuiOutlinedInput-root.Mui-focused': {
-              '& fieldset': {
-                borderColor: '#cad3dd', 
+            "& .MuiOutlinedInput-root.Mui-focused": {
+              "& fieldset": {
+                borderColor: "#cad3dd",
               },
             },
           },
@@ -44,188 +44,189 @@ function Budget() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.put(`http://localhost:2309/event/${eventId}`, {
-      budget: budget,
-      details: details,
-    })
-    .then((response) => {
-      console.log("update response: ", response);
-      console.log("update data: ", response.data);
-      navigate(`/success?userId=${userId}&eventId=${eventId}`)
-    })
-  }
+    axios
+      .put(`http://localhost:2309/event/${eventId}`, {
+        budget: budget,
+        details: details,
+      })
+      .then((response) => {
+        console.log("update response: ", response);
+        console.log("update data: ", response.data);
+        navigate(`/success?userId=${userId}&eventId=${eventId}`);
+      });
+  };
 
   const handleClick = () => {
     navigate(`/eventcreate?userId=${userId}`);
-  }
+  };
 
-  return <Box 
-      backgroundColor='#FFFDE3'
-      width='100vw'
-      p='50px 30px 33px 30px'
-    >
-      <Box 
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: '1rem',
-      }}
-      onClick={handleClick}
-    >
-      <ArrowCircleLeftOutlinedIcon 
+  return (
+    <Box backgroundColor="#FFFDE3" width="100vw" p="50px 30px 33px 30px">
+      <Box
         sx={{
-          fontSize: '1.2rem',
-          color: '#C21010'
+          display: "flex",
+          alignItems: "center",
+          marginBottom: "1rem",
         }}
-      />
-      <Button
+        onClick={handleClick}
+      >
+        <ArrowCircleLeftOutlinedIcon
+          sx={{
+            fontSize: "1.2rem",
+            color: "#C21010",
+          }}
+        />
+        <Button
+          sx={{
+            textTransform: "inherit",
+            color: "#C21010",
+            fontWeight: "bold",
+            fontSize: "1rem",
+          }}
+        >
+          Step1
+        </Button>
+      </Box>
+      <Box
         sx={{
-          textTransform: 'inherit',
-          color: '#C21010',
-          fontWeight: 'bold',
-          fontSize: '1rem'
-        }}
-      >Step1</Button>
-    </Box>
-    <Box 
-      sx={{
-        minHeight: '80vh',
-        background: '#fff',
-        border: '1px solid #e8ecf1',
-        borderRadius: '10px'
-      }}
-    >
-      <Box 
-        sx={{
-          maxWidth: '517px',
-          margin: '0 auto',
-          padding: '48px 10px 16px',
+          minHeight: "80vh",
+          background: "#fff",
+          border: "1px solid #e8ecf1",
+          borderRadius: "10px",
         }}
       >
-        <Box 
+        <Box
           sx={{
-            paddingBottom: '28px',
-            marginBottom: '28px',
-            borderBottom: '1px solid #cad3dd',
-            textAlign: 'center'
+            maxWidth: "517px",
+            margin: "0 auto",
+            padding: "48px 10px 16px",
           }}
         >
-          <img 
-            src={Gift} 
-            alt='Gift image' 
-            height={100} 
-            width={100}
-            className='starimage'
-          />
-          <Typography
-            variant='h4'
+          <Box
             sx={{
-              margin:'0 0 20px',
-              color: '#C21010'
+              paddingBottom: "28px",
+              marginBottom: "28px",
+              borderBottom: "1px solid #cad3dd",
+              textAlign: "center",
             }}
           >
-            Gift exchange rules
-          </Typography>
-      </Box>
-      <Box>
-        <Box 
-          sx={{
-            marginBottom: '20px',
-          }}
-        >
-          <Typography 
-            sx={{
-              fontFamily: 'Poppins,sans-serif',
-              fontWeight: 600,
-              fontSize: '13px',
-              lineHeight: '18px',
-              color: '#C21010',
-              wordBreak: 'break-all',
-            }}
-          >
-            Spending limit per gift
-          </Typography>
-          <ThemeProvider theme={theme}>
-            <TextField 
-              placeholder='Enter Min Budget'
-              sx={{
-                width: '100%',
-                background:'#fff',
-                borderRadius: '7px',
-                padding: '8px 15px',
-                fontWeight: 400,
-                fontSize:"1rem",
-                color: '#101a34',
-                lineHeight: '27px',
-                '& .MuiOutlinedInput-root': {
-                  height: '44px',
-              },
-              marginLeft: '-13px'
-              }}
-              value={budget}
-              onChange={(e) => setBudget(e.target.value)}
-            /> 
-          </ThemeProvider>
-        </Box>
-        <Box 
-          sx={{
-            marginBottom: '20px',
-          }}
-        >
-          <Typography
-             sx={{
-              fontFamily: 'Poppins,sans-serif',
-              fontWeight: 600,
-              fontSize: '13px',
-              lineHeight: '18px',
-              color: '#C21010',
-              wordBreak: 'break-all',
-            }}
-          >
-            Other details(Optional)
-          </Typography>
-          <ThemeProvider theme={theme}>
-            <TextField 
-                placeholder='Enter your text'
-                multiline
-                rows={4}
-                sx={{
-                  width: '100%',
-                }}
-                value={details}
-                onChange={(e) => setdetails(e.target.value)}
+            <img
+              src={Gift}
+              alt="Gift image"
+              height={100}
+              width={100}
+              className="starimage"
             />
-          </ThemeProvider>
+            <Typography
+              variant="h4"
+              sx={{
+                margin: "0 0 20px",
+                color: "#C21010",
+              }}
+            >
+              Gift exchange rules
+            </Typography>
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                marginBottom: "20px",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontFamily: "Poppins,sans-serif",
+                  fontWeight: 600,
+                  fontSize: "13px",
+                  lineHeight: "18px",
+                  color: "#C21010",
+                  wordBreak: "break-all",
+                }}
+              >
+                Spending limit per gift
+              </Typography>
+              <ThemeProvider theme={theme}>
+                <TextField
+                  placeholder="Enter Min Budget"
+                  sx={{
+                    width: "100%",
+                    background: "#fff",
+                    borderRadius: "7px",
+                    padding: "8px 15px",
+                    fontWeight: 400,
+                    fontSize: "1rem",
+                    color: "#101a34",
+                    lineHeight: "27px",
+                    "& .MuiOutlinedInput-root": {
+                      height: "44px",
+                    },
+                    marginLeft: "-13px",
+                  }}
+                  value={budget}
+                  onChange={(e) => setBudget(e.target.value)}
+                />
+              </ThemeProvider>
+            </Box>
+            <Box
+              sx={{
+                marginBottom: "20px",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontFamily: "Poppins,sans-serif",
+                  fontWeight: 600,
+                  fontSize: "13px",
+                  lineHeight: "18px",
+                  color: "#C21010",
+                  wordBreak: "break-all",
+                }}
+              >
+                Other details(Optional)
+              </Typography>
+              <ThemeProvider theme={theme}>
+                <TextField
+                  placeholder="Enter your text"
+                  multiline
+                  rows={4}
+                  sx={{
+                    width: "100%",
+                  }}
+                  value={details}
+                  onChange={(e) => setdetails(e.target.value)}
+                />
+              </ThemeProvider>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                onClick={handleSubmit}
+                sx={{
+                  border: "2px solid #C21010",
+                  borderRadius: "10px",
+                  width: "100%",
+                  fontSize: "1rem",
+                  color: "white",
+                  background: "#C21010",
+                  textTransform: "inherit",
+                  fontWeight: "bold",
+                  "&:hover": {
+                    backgroundColor: "#fff",
+                    color: "#C21010",
+                    border: "1px solid #C21010",
+                  },
+                  marginTop: "10px",
+                }}
+              >
+                Submit
+              </Button>
+            </Box>
+          </Box>
         </Box>
-        <Box >
-          <Button 
-            variant='contained'
-            onClick={handleSubmit}
-            sx={{ 
-              border: '2px solid #C21010',
-              borderRadius: '10px',
-              width: '100%',
-              fontSize: '1rem',
-              color: 'white',
-              background: '#C21010',
-              textTransform: 'inherit',
-              fontWeight:'bold',
-              '&:hover': {
-                backgroundColor: '#fff',
-                color: '#C21010',
-                border: '1px solid #C21010'
-              },
-              marginTop: '10px'
-            }}
-          >
-            Submit
-          </Button>
-        </Box>
+      </Box>
+      <Footer />
     </Box>
-    </Box>
-    </Box>
-    <Footer />
-  </Box>
+  );
 }
 
-export default Budget
+export default Budget;
