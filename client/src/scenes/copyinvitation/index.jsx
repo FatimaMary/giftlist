99aa3125ed7d8b07b7056e496b14a750b61ad93a
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-05 15:27:50 +0530

    Update index.jsx

--- client/src/scenes/copyinvitation/index.jsx
+++ client/src/scenes/copyinvitation/index.jsx
@@ -1,40 +1,52 @@
-import React, { useRef, useState } from 'react';
-import { Box, Typography, Button, TextField, createTheme, ThemeProvider } from '@mui/material';
-import copy from 'copy-to-clipboard';
+import React, { useRef, useState } from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import copy from "copy-to-clipboard";
 
-
-function CopyInvitation({ firstName, rsvpDate, eventName, eventId, setInvitePage }) {
-    const [copyText, setCopyText] = useState("");
-    const textFieldRef = useRef(null);
-    const copyToClipboard = () => {
-        copy(textFieldRef.current.defaultValue);
-        setCopyText(textFieldRef.current.defaultValue);
-        setInvitePage(false);
-    }
-    const theme = createTheme({
-        components: {
-          MuiTextField: {
-            styleOverrides: {
-              root: {
-                '& .MuiOutlinedInput-root': {
-                  '& fieldset': {
-                    border: '1px solid grey', 
-                  },
-                },
-                '& .MuiOutlinedInput-root.Mui-focused': {
-                  '& fieldset': {
-                    borderColor: '1px solid grey', 
-                  },
-                },
+function CopyInvitation({
+  firstName,
+  rsvpDate,
+  eventName,
+  eventId,
+  setInvitePage,
+}) {
+  const [copyText, setCopyText] = useState("");
+  const textFieldRef = useRef(null);
+  const copyToClipboard = () => {
+    copy(textFieldRef.current.defaultValue);
+    setCopyText(textFieldRef.current.defaultValue);
+    setInvitePage(false);
+  };
+  const theme = createTheme({
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                border: "1px solid grey",
+              },
+            },
+            "& .MuiOutlinedInput-root.Mui-focused": {
+              "& fieldset": {
+                borderColor: "1px solid grey",
               },
             },
           },
         },
-      });
+      },
+    },
+  });
 
   return (
     <Box>
-        <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
         <TextField
           id="outlined-multiline-static"
           multiline
@@ -45,32 +57,30 @@ function CopyInvitation({ firstName, rsvpDate, eventName, eventId, setInvitePage
           
           Click on the link to join  
           http://localhost:3000/eventview1?eventId=${eventId}`}
-          sx={{ width: '100%', border: 'none', }}
+          sx={{ width: "100%", border: "none" }}
         />
-        </ThemeProvider>
-        <Box
-         sx={{display: 'flex', justifyContent: 'center'}}
+      </ThemeProvider>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Button
+          onClick={copyToClipboard}
+          variant="outlined"
+          sx={{
+            border: "1px solid #C21010",
+            width: "200px",
+            borderRadius: "10px",
+            textTransform: "inherit",
+            marginBottom: "5px",
+            color: "#C21010",
+            background: "#fff",
+            fontWeight: "bold",
+            marginTop: "5px",
+          }}
         >
-            <Button 
-                onClick={copyToClipboard}
-                variant='outlined'
-                sx={{ 
-                    border: '1px solid #C21010', 
-                    width: '200px',
-                    borderRadius: '10px',
-                    textTransform: 'inherit',
-                    marginBottom: '5px',
-                    color: '#C21010',
-                    background: '#fff',
-                    fontWeight: 'bold',
-                    marginTop: '5px'
-                }}
-            >
-                Copy
-            </Button>
-        </Box>
+          Copy
+        </Button>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default CopyInvitation
+export default CopyInvitation;
