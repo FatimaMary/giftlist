fb11944c9c231ef43604f82bb96f5735f1545381
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 14:53:24 +0530

    Update Participants.js

--- server/models/Participants.js
+++ server/models/Participants.js
@@ -1,37 +1,37 @@
-import mongoose from 'mongoose';
-import autoIncrement from 'mongoose-plugin-autoinc';
+import mongoose from "mongoose";
+import autoIncrement from "mongoose-plugin-autoinc";
 
 const participantsSchema = new mongoose.Schema({
-    participantsId : {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    participantsEmail : {
-        type: String,
-    },
-    participantsAcceptence: {
-        type: Boolean,
-        required: true,
-    },
-    eventId : {
-        type: String,
-        required: true,
-    },
-    userId : {
-        type: String,
-    },
-    wishList : {
-        type: Array,
-        default: [],
-    },
+  participantsId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  participantsEmail: {
+    type: String,
+  },
+  participantsAcceptence: {
+    type: Boolean,
+    required: true,
+  },
+  eventId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+  },
+  wishList: {
+    type: Array,
+    default: [],
+  },
 });
 
 participantsSchema.plugin(autoIncrement.plugin, {
-    model: "GiftlistParticipants",
-    field: "participantsId",
-    startAt: 1,
-    incrementBy: 1,
+  model: "GiftlistParticipants",
+  field: "participantsId",
+  startAt: 1,
+  incrementBy: 1,
 });
 
 const Participants = mongoose.model("GiftlistParticipants", participantsSchema);
