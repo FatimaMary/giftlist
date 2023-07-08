d8354ecaeef77cdd556ad4169a8ce0a00e9d1e1f
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 14:54:30 +0530

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    firstName: {
-        type: String,
-    },
-    secondName: {
-        type: String,
-    },
-    birthDay: {
-        type: Date
-    },
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  secondName: {
+    type: String,
+  },
+  birthDay: {
+    type: Date,
+  },
 });
 
 const Users = mongoose.model("GiftlistUsers", userSchema);
 
-export default Users; 
+export default Users;
