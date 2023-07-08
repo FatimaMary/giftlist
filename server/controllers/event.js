805bdcce62b03024f21500762795964c31d9ea6e
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 13:52:02 +0530

    Update event.js

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,77 +1,77 @@
-import Events from '../models/Event.js';
-import Users from '../models/Users.js'
+import Events from "../models/Event.js";
+import Users from "../models/Users.js";
 
 export const postEvent = (req, res) => {
-    const eventName = req.body.eventName;
-    const giftExchangeDate = req.body.giftExchangeDate;
-    const rsvpDate = req. body.rsvpDate;
-    const confirmation = req.body.confirmation;
-    const drawNames = 'false';
-    const userId = req.body.userId;
+  const eventName = req.body.eventName;
+  const giftExchangeDate = req.body.giftExchangeDate;
+  const rsvpDate = req.body.rsvpDate;
+  const confirmation = req.body.confirmation;
+  const drawNames = "false";
+  const userId = req.body.userId;
 
-    const newEvent = new Events({
-       eventName,
-       giftExchangeDate,
-       rsvpDate,
-       confirmation,
-       drawNames,
-       userId,
-    });
+  const newEvent = new Events({
+    eventName,
+    giftExchangeDate,
+    rsvpDate,
+    confirmation,
+    drawNames,
+    userId,
+  });
 
-    newEvent
-        .save()
-        .then((data) => res.json(data))
-        .catch((err) => res.status(400).json("Error: " + err));
+  newEvent
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 export const getAllEvents = (req, res) => {
-    Events.find()
-        .then((events) => res.json(events))
-        .catch((err) => res.status(400).json("Error: " + err));
+  Events.find()
+    .then((events) => res.json(events))
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 export const updateEvent = (req, res) => {
-    const eventId = req.params.eventId;
-    Events.findOne({ eventId: eventId })
+  const eventId = req.params.eventId;
+  Events.findOne({ eventId: eventId })
     .then((event) => {
-        event.budget = req.body.budget;
-        event.details = req.body.details;
-        event
-            .save()
-            .then(() => res.json("Event Updated"))
-            .catch((err) => res.status(400).json("Error: " + err));
+      event.budget = req.body.budget;
+      event.details = req.body.details;
+      event
+        .save()
+        .then(() => res.json("Event Updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
     })
     .catch((err) => res.status(400).json("Error: " + err));
 };
 
 export const getEventsById = (req, res) => {
-    const userId = req.params.userId;
-    Events.find({ userId: userId })
-      .then((events) => {
-        if (events.length === 0) {
-          res.status(404).json({ message: 'No event found' });
-        } else {
-          const eventDetails = events.map(event => {
-            return {
-              eventId: event.eventId,
-              eventName: event.eventName,
-              giftExchangeDate: event.giftExchangeDate,
-            };
-          });
-          res.json(eventDetails);
-        }
-      })
-      .catch((err) => res.status(400).json({ message: err.message }));
-}
+  const userId = req.params.userId;
+  Events.find({ userId: userId })
+    .then((events) => {
+      if (events.length === 0) {
+        res.status(404).json({ message: "No event found" });
+      } else {
+        const eventDetails = events.map((event) => {
+          return {
+            eventId: event.eventId,
+            eventName: event.eventName,
+            giftExchangeDate: event.giftExchangeDate,
+          };
+        });
+        res.json(eventDetails);
+      }
+    })
+    .catch((err) => res.status(400).json({ message: err.message }));
+};
 
 export const getEventDetailsbyEventId = (req, res) => {
   const eventId = req.params.eventId;
   Events.find({ eventId: eventId })
     .then((events) => {
       if (events.length === 0) {
-        res.status(404).json({ message: 'There is no event details' });
+        res.status(404).json({ message: "There is no event details" });
       } else {
-        const eventDetail = events.map(singleEvent => {
+        const eventDetail = events.map((singleEvent) => {
           return {
             eventName: singleEvent.eventName,
             giftExchangeDate: singleEvent.giftExchangeDate,
@@ -80,7 +80,7 @@ export const getEventDetailsbyEventId = (req, res) => {
             details: singleEvent.details,
             userId: singleEvent.userId,
             drawNames: singleEvent.drawNames,
-            drawnNames: singleEvent.drawnNames
+            drawnNames: singleEvent.drawnNames,
           };
         });
         res.json(eventDetail[0]);
@@ -93,21 +93,21 @@ export const getuserNameByEventId = (req, res) => {
   const eventId = req.params.eventId;
   Events.find({ eventId: eventId })
     .then((events) => {
-      if(events.length === 0) {
-        res.status(404).json({ message: 'There is no event details' });
+      if (events.length === 0) {
+        res.status(404).json({ message: "There is no event details" });
       } else {
-        const eventDetail = events.map((singleEvent) => singleEvent.userId)
-        
+        const eventDetail = events.map((singleEvent) => singleEvent.userId);
+
         Users.find({ userId: { $in: eventDetail } })
           .then((users) => {
-            if(users.length === 0) {
-              res.status(404).json({ message: 'No user' });
+            if (users.length === 0) {
+              res.status(404).json({ message: "No user" });
             } else {
               const userDetail = users.map((user) => {
                 return {
                   firstName: user.firstName,
-                  secondName: user.secondName
-                }
+                  secondName: user.secondName,
+                };
               });
               console.log("userDetail: ", userDetail);
               res.json(userDetail[0]);
@@ -122,43 +122,43 @@ export const getuserNameByEventId = (req, res) => {
 export const editEvent = (req, res) => {
   const eventId = req.params.eventId;
   Events.findOne({ eventId: eventId })
-  .then((event) => {
+    .then((event) => {
       event.eventName = req.body.eventName;
       event.giftExchangeDate = req.body.giftExchangeDate;
       event.rsvpDate = req.body.rsvpDate;
       event.budget = req.body.budget;
       event.details = req.body.details;
       event
-          .save()
-          .then(() => res.json("Event Updated"))
-          .catch((err) => res.status(400).json("Error: " + err));
-  })
-  .catch((err) => res.status(400).json("Error: " + err));
+        .save()
+        .then(() => res.json("Event Updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 };
 
 export const deleteEvent = (req, res) => {
   const eventId = req.params.eventId;
-  Events.findOneAndDelete({ eventId: eventId})
-  .then((deletedEvent) => {
-    if (!deletedEvent) {
-      return res.status(404).json({ error: 'Event not found' });
-    }
-    return res.status(200).json({ message: 'Event deleted successfully' });
-  })
-  .catch((err) => {
-    return res.status(500).json({ error: 'Failed to delete event' });
-  });
+  Events.findOneAndDelete({ eventId: eventId })
+    .then((deletedEvent) => {
+      if (!deletedEvent) {
+        return res.status(404).json({ error: "Event not found" });
+      }
+      return res.status(200).json({ message: "Event deleted successfully" });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: "Failed to delete event" });
+    });
 };
 
 export const updateDrawNames = (req, res) => {
   const eventId = req.params.eventId;
   Events.findOne({ eventId: eventId })
-  .then((event) => {
+    .then((event) => {
       event.drawNames = req.body.drawNames;
       event
-          .save()
-          .then(() => res.json("Event Updated"))
-          .catch((err) => res.status(400).json("Error: " + err));
-  })
-  .catch((err) => res.status(400).json("Error: " + err));
+        .save()
+        .then(() => res.json("Event Updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 };
