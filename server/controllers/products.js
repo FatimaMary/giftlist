e060ec8a12e9b3a409f79fe68450a5babd6c07a5
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 17:09:15 +0530

    Update products.js

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -22,3 +22,32 @@ export const postProduct = (req, res) => {
     .then((data) => res.json(data))
     .catch((err) => res.status(404).json("Error: ", err));
 };
+
+export const getAllProducts = (req, res) => {
+  Products.find()
+    .then((products) => res.json(products))
+    .catch((err) => res.status(404).json("Error: ", err));
+};
+
+export const getProductsByParticipantsId = (req, res) => {
+  const participantsId = req.params.participantsId;
+  Products.findOne({ participantsId: participantsId })
+    .then((products) => {
+      if (products.length === 0) {
+        res.status(200).json("No Products Found");
+      } else {
+        const productDetails = products.map((product) => {
+          return {
+            productId: product.productId,
+            productName: product.productName,
+            productPrice: product.productPrice,
+            description: product.description,
+            eventId: product.eventId,
+            participantsId: product.participantsId,
+          };
+        });
+        res.json(productDetails);
+      }
+    })
+    .catch((err) => res.status(400).json({ message: err.message }));
+};
