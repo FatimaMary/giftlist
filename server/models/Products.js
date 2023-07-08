4ab64d4f73a0a3e95f2b40c1d4c3c0a81411f2b2
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 13:27:01 +0530

    Create Products.js

---
+++ server/models/Products.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import autoIncrement from "mongoose-plugin-autoinc";
+
+const productsSchema = new mongoose.Schema({
+  productId: {
+    type: String,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  productUrl: {
+    type: String,
+    required: true,
+  },
+  productPrice: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  eventId: {
+    type: String,
+    required: true,
+  },
+  participantsId: {
+    type: String,
+    required: true,
+  },
+});
+
+productsSchema.plugin(autoIncrement.plugin, {
+  model: "GiftlistProducts",
+  field: "productId",
+  startAt: 1,
+  incrementBy: 1,
+});
+
+const Products = mongoose.model("GiftlistProducts", productsSchema);
+
+export default Products;
