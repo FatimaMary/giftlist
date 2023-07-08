4e89d577020a4f29fff44e5d5693e4319e09412e
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-08 14:53:54 +0530

    Update Event.js

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,56 +1,56 @@
 import mongoose from "mongoose";
-import autoIncrement from "mongoose-plugin-autoinc"; 
+import autoIncrement from "mongoose-plugin-autoinc";
 
 const eventSchema = new mongoose.Schema({
-    eventId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    eventName: {
-        type: String,
-        required: true
-    },
-    giftExchangeDate: {
-        type: String,
-        required: true
-    },
-    rsvpDate: {
-        type: String,
-        required: true,
-    },
-    confirmation: {
-        type: Boolean,
-        required: true,
-    },
-    budget: {
-        type: String,
-    },
-    details: {
-        type: String,
-    },
-    drawNames : {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    drawnNames : {
-        type: Array,
-        default: [],
-    },
-    userId: {
-        type: String,
-        required: true
-    },
+  eventId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  eventName: {
+    type: String,
+    required: true,
+  },
+  giftExchangeDate: {
+    type: String,
+    required: true,
+  },
+  rsvpDate: {
+    type: String,
+    required: true,
+  },
+  confirmation: {
+    type: Boolean,
+    required: true,
+  },
+  budget: {
+    type: String,
+  },
+  details: {
+    type: String,
+  },
+  drawNames: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  drawnNames: {
+    type: Array,
+    default: [],
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
 });
 
 eventSchema.plugin(autoIncrement.plugin, {
-    model: "GiftlistEvents",
-    field: "eventId",
-    startAt: 1,
-    incrementBy: 1,
+  model: "GiftlistEvents",
+  field: "eventId",
+  startAt: 1,
+  incrementBy: 1,
 });
 
 const Events = mongoose.model("GiftlistEvents", eventSchema);
 
-export default Events; 
+export default Events;
