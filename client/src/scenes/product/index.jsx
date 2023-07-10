cce75edd776563df3b08e4470bbe6fcf0fb10ba6
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-10 16:18:49 +0530

    Update index.jsx

--- client/src/scenes/product/index.jsx
+++ client/src/scenes/product/index.jsx
@@ -1,9 +1,53 @@
+// import React, { useState } from "react";
+// import { Box, Typography, Button, TextField } from "@mui/material";
+// import axios from "axios";
+
+// function Product() {
+//   const [productUrl, setProductUrl] = useState("");
+
+//   const handleUrlChange = (e) => {
+//     setProductUrl(e.target.value);
+//   };
+
+//   const fetchProductDetails = async (url) => {
+//     try {
+//       const response = await axios.get(url);
+//       const productDetails = response.data;
+//       console.log(productDetails);
+//     } catch (error) {
+//       console.error("Error fetching product details:", error);
+//     }
+//   };
+
+//   const handleFetchProduct = () => {
+//     fetchProductDetails(productUrl);
+//   };
+
+//   return (
+//     <Box>
+//       <Typography>Product Details</Typography>
+//       <TextField
+//         sx={{
+//           width: "200px",
+//         }}
+//         placeholder="enter product url"
+//         value={productUrl}
+//         onClick={handleUrlChange}
+//       ></TextField>
+//       <Button onClick={handleFetchProduct}>Submit</Button>
+//     </Box>
+//   );
+// }
+
+// export default Product;
+
 import React, { useState } from "react";
 import { Box, Typography, Button, TextField } from "@mui/material";
 import axios from "axios";
 
 function Product() {
-  const [productUrl, setProductUrl] = useState();
+  const [productUrl, setProductUrl] = useState("");
+
   const fetchProductDetails = async (url) => {
     try {
       const response = await axios.get(url);
@@ -15,8 +59,24 @@ function Product() {
   };
 
   const handleFetchProduct = () => {
-    const productUrl = "https://example.com/product/123";
-    fetchProductDetails(productUrl);
+    if (isValidUrl(productUrl)) {
+      fetchProductDetails(productUrl);
+    } else {
+      console.error("Invalid URL:", productUrl);
+    }
+  };
+
+  const handleUrlChange = (e) => {
+    setProductUrl(e.target.value);
+  };
+
+  const isValidUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
+    }
   };
 
   return (
@@ -26,8 +86,10 @@ function Product() {
         sx={{
           width: "200px",
         }}
-        placeholder="enter product url"
-      ></TextField>
+        placeholder="Enter product URL"
+        value={productUrl}
+        onChange={handleUrlChange}
+      />
       <Button onClick={handleFetchProduct}>Submit</Button>
     </Box>
   );
