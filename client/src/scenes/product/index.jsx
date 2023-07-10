6b3463b4c725ef5aee6bd8150e3dc868f3eacf78
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-10 15:18:14 +0530

    Update index.jsx

--- client/src/scenes/product/index.jsx
+++ client/src/scenes/product/index.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Typography, Button, TextField } from "@mui/material";
+import axios from "axios";
 
 function Product() {
-  return <div>Product</div>;
+  const [productUrl, setProductUrl] = useState();
+  const fetchProductDetails = async (url) => {
+    try {
+      const response = await axios.get(url);
+      const productDetails = response.data;
+      console.log(productDetails);
+    } catch (error) {
+      console.error("Error fetching product details:", error);
+    }
+  };
+
+  const handleFetchProduct = () => {
+    const productUrl = "https://example.com/product/123";
+    fetchProductDetails(productUrl);
+  };
+
+  return (
+    <Box>
+      <Typography>Product Details</Typography>
+      <TextField
+        sx={{
+          width: "200px",
+        }}
+        placeholder="enter product url"
+      ></TextField>
+      <Button onClick={handleFetchProduct}>Submit</Button>
+    </Box>
+  );
 }
 
 export default Product;
