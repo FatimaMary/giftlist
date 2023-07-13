9962344a89c61447360411b57537608853c732ca
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-13 09:53:11 +0530

    Update index.jsx

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,26 +3,21 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../Components/Sidebar";
 
-
 const Layout = () => {
-    const isNonMobile = useMediaQuery("(min-width: 600px)");
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-    return (
-        <Box 
-            display={isNonMobile ? "flex" : "block"}
-            width='100%'
-            height='100%'
-        >
-            <Sidebar 
-                isNonMobile={isNonMobile}
-                drawerWidth="230px"
-                isSidebarOpen={isSidebarOpen}
-                setIsSidebarOpen={setIsSidebarOpen}
-            />
-            <Outlet />
-        </Box>
-    )
-}
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="230px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Outlet />
+    </Box>
+  );
+};
 
 export default Layout;
