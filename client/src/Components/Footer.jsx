e10b718c8cfe434552a078126114379a3e3412fd
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-13 09:57:57 +0530

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,95 +1,101 @@
-import React from 'react';
-import CopyrightIcon from '@mui/icons-material/Copyright';
-import { 
-    Box, 
-    Button, 
-    TextField, 
-    ThemeProvider, 
-    Typography,
-    createTheme,
-  } from '@mui/material';
+import React from "react";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import {
+  Box,
+  Button,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 
 function Footer() {
-  return <Box sx={{
-    display: 'flex',
-    justifyContent:'space-between',
-    borderTop: '1px solid grey',
-    marginTop: '20px',
-  }}>
-    <Box sx={{
-      display: 'flex',
-      paddingTop: '10px',
-    }}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        borderTop: "1px solid grey",
+        marginTop: "20px",
+      }}
     >
-      <CopyrightIcon 
+      <Box
         sx={{
-          fontSize: '16px',
-          color: '#C21010'
-        }} 
-      />
-      <Typography sx={{
-          fontSize: '13px',
-          color: '#C21010',
-        }} 
+          display: "flex",
+          paddingTop: "10px",
+        }}
       >
-        GiftList. All Rights Reserved
-      </Typography>
+        <CopyrightIcon
+          sx={{
+            fontSize: "16px",
+            color: "#C21010",
+          }}
+        />
+        <Typography
+          sx={{
+            fontSize: "13px",
+            color: "#C21010",
+          }}
+        >
+          GiftList. All Rights Reserved
+        </Typography>
+      </Box>
+      <Box>
+        <Button
+          sx={{
+            textTransform: "inherit",
+            color: "#C21010",
+            "&:hover": {
+              cursor: "pointer",
+              color: "skyblue",
+              background: "transparent",
+            },
+          }}
+        >
+          Privacy policy
+        </Button>
+        <Button
+          sx={{
+            textTransform: "inherit",
+            color: "#C21010",
+            "&:hover": {
+              cursor: "pointer",
+              color: "skyblue",
+              background: "transparent",
+            },
+          }}
+        >
+          Terms of use
+        </Button>
+        <Button
+          sx={{
+            textTransform: "inherit",
+            color: "#C21010",
+            "&:hover": {
+              cursor: "pointer",
+              color: "skyblue",
+              background: "transparent",
+            },
+          }}
+        >
+          Desclaimer
+        </Button>
+        <Button
+          sx={{
+            textTransform: "inherit",
+            color: "#C21010",
+            "&:hover": {
+              cursor: "pointer",
+              color: "skyblue",
+              background: "transparent",
+            },
+          }}
+        >
+          Contact Us
+        </Button>
+      </Box>
     </Box>
-    <Box>
-      <Button 
-        sx={{
-          textTransform: 'inherit',
-           color: '#C21010',
-          '&:hover': {
-            cursor: 'pointer',
-            color: 'skyblue',
-            background: 'transparent'
-          },
-        }}
-      >
-        Privacy policy
-      </Button>
-      <Button
-        sx={{
-          textTransform: 'inherit',
-           color: '#C21010',
-          '&:hover': {
-            cursor: 'pointer',
-            color: 'skyblue',
-            background: 'transparent'
-          },
-        }}
-      >
-        Terms of use
-      </Button>
-      <Button
-        sx={{
-          textTransform: 'inherit',
-           color: '#C21010',
-          '&:hover': {
-            cursor: 'pointer',
-            color: 'skyblue',
-            background: 'transparent'
-          },
-        }}
-      >
-        Desclaimer
-      </Button>
-      <Button 
-        sx={{
-          textTransform: 'inherit',
-           color: '#C21010',
-          '&:hover': {
-            cursor: 'pointer',
-            color: 'skyblue',
-            background: 'transparent'
-          },
-        }}
-      >
-        Contact Us
-      </Button>
-    </Box>
-  </Box>
+  );
 }
 
-export default Footer
+export default Footer;
