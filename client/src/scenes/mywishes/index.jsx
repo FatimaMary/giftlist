d59b4ded9b673e69ac4b4812c83abc72a51e73f7
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-13 17:00:26 +0530

    Update index.jsx

--- client/src/scenes/mywishes/index.jsx
+++ client/src/scenes/mywishes/index.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 function MyWishes() {
-  return <div>MyWishes</div>;
+  const theme = createTheme({
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "grey",
+              },
+            },
+            "& .MuiOutlinedInput-root.Mui-focused": {
+              "& fieldset": {
+                borderColor: "grey",
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  return (
+    <Box
+      sx={{
+        padding: "15px 0",
+        marginBottom: "20px",
+      }}
+    >
+      <Typography
+        sx={{
+          fontWeight: 600,
+          fontSize: "13px",
+          lineHeight: "18px",
+          color: "#101a34",
+          marginBottom: "5px",
+          wordBreak: "break-all",
+        }}
+      >
+        Add an item
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "space-between",
+          marginBottom: 0,
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <TextField
+            placeholder="Enter a URL"
+            sx={{
+              maxWidth: "calc(100% - 130px)",
+              flexBasis: "calc(100% - 130px)",
+              background: "#fff",
+              borderRadius: "10px",
+              fontWeight: 400,
+              fontSize: "16px",
+              color: "#101a34",
+              lineHeight: "27px",
+              "& .MuiOutlinedInput-root": {
+                height: "44px",
+              },
+            }}
+          />
+        </ThemeProvider>
+        <Button>
+          <AddCircleOutlineIcon /> Add
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default MyWishes;
