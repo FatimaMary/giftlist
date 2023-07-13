1c713e85ff6783ac60948a72ad619ca37990995d
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-13 09:57:00 +0530

    Update invite.jsx

--- client/src/scenes/success/invite.jsx
+++ client/src/scenes/success/invite.jsx
@@ -1,55 +1,51 @@
-import React from 'react';
-import { 
-    Box, 
-    Typography, 
-    Button,
-    Dialog,
-    DialogContent,
-} from '@mui/material';
-import CopyInvitation from '../copyinvitation';
+import React from "react";
+import { Box, Typography, Button, Dialog, DialogContent } from "@mui/material";
+import CopyInvitation from "../copyinvitation";
 
-function Invite({ 
-  open, 
-  onClose, 
-  invitePage, 
-  setInvitePage, 
+function Invite({
+  open,
+  onClose,
+  invitePage,
+  setInvitePage,
   firstName,
   eventName,
   rsvpDate,
   eventId,
 }) {
-    const handleClose = () => {
-        onClose(invitePage);
-    }
-  return <Dialog
-    sx={{
-        margin: '0 361px',
-        display: 'flex',
-    }}
-    onClose={handleClose}
-    open={open}
-  >
-    <DialogContent 
-        sx={{
-            background: '#fff',
-            boxShadow: '5px 5px 25px -5px rgba(32, 32, 36, .1)',
-            borderRadius: '10px',
-            padding: '24px 20px',
-            display: 'flex',
-            flexDirection: 'column',
-            width: '500px',
-            justifyContent: 'center',
-        }}
+  const handleClose = () => {
+    onClose(invitePage);
+  };
+  return (
+    <Dialog
+      sx={{
+        margin: "0 361px",
+        display: "flex",
+      }}
+      onClose={handleClose}
+      open={open}
     >
-            <CopyInvitation 
-              setInvitePage={setInvitePage}
-              firstName={firstName}
-              eventId={eventId}
-              eventName={eventName}
-              rsvpDate={rsvpDate}
-            />
-    </DialogContent>
-  </Dialog>
+      <DialogContent
+        sx={{
+          background: "#fff",
+          boxShadow: "5px 5px 25px -5px rgba(32, 32, 36, .1)",
+          borderRadius: "10px",
+          padding: "24px 20px",
+          display: "flex",
+          flexDirection: "column",
+          width: "500px",
+          justifyContent: "center",
+        }}
+      >
+        <CopyInvitation
+          setInvitePage={setInvitePage}
+          firstName={firstName}
+          eventId={eventId}
+          eventName={eventName}
+          rsvpDate={rsvpDate}
+        />
+      </DialogContent>
+    </Dialog>
+  );
 }
 
-export default Invite
+export default Invite;
