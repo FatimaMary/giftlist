87cd94c1c045c4f7c8f27588a4799da4ecbc664b
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-17 18:35:30 +0530

    Update products.js

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,6 +4,7 @@ import {
   getAllProducts,
   getProductsByParticipantsId,
   getProductsByEventId,
+  getWishlist,
 } from "../controllers/products.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/add", postProduct);
 router.get("/all", getAllProducts);
 router.get("/all/:participantsId", getProductsByParticipantsId);
 router.get("/details/:eventId", getProductsByEventId);
+router.get("/wishlist/:participantsId", getWishlist);
 
 export default router;
