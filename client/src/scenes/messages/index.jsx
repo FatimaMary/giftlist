4f4e24b84a1af2699286b610175cd963fbc5b676
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-24 17:44:55 +0530

    Update index.jsx

--- client/src/scenes/messages/index.jsx
+++ client/src/scenes/messages/index.jsx
@@ -1,8 +1,57 @@
 import React from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 function Messages() {
-  return <div>Messages</div>;
+  return (
+    <Box
+      sx={{
+        padding: "15px 0",
+      }}
+    >
+      <Box
+        sx={{
+          marginBottom: "16px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 600,
+            fontSize: "13px",
+            lineHeight: "18px",
+            color: "#101a34",
+            marginBottom: "5px",
+            wordBreak: "break-all",
+          }}
+        >
+          Send a message to the group
+        </Typography>
+        <Box
+          sx={{
+            marginBottom: "20px",
+          }}
+        >
+          <TextField
+            placeholder="Enter your message text"
+            sx={{
+              height: "46px",
+              background: "#fff",
+              borderRadius: "7px",
+              padding: "8px 15px",
+              fontWeight: 400,
+              fontSize: "16px",
+              color: "#101a34",
+              border: "1px solid #cad3dd",
+            }}
+          />
+          <Button>
+            <MailOutlineIcon />
+            Send message
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Messages;
