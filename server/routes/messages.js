77d76db2b8290d6d45e4457f6fd82b11ed09c392
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-26 18:23:34 +0530

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { postMessage } from "../controllers/message.js";
 
 const router = express.Router();
+
+router.post("/add", postMessage);
 
 export default router;
