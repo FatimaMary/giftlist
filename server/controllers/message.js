a65fe18e1190c004459f8cd71f41e627bba6a8f5
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-26 17:34:35 +0530

    Create message.js

---
+++ server/controllers/message.js
@@ -0,0 +1,21 @@
+import Messages from "../models/Message.js";
+
+const postMessage = (req, res) => {
+  const message = req.body.message;
+  const userId = req.body.userId;
+  const eventId = req.body.eventId;
+  const participantsId = req.body.participantsId;
+  const timeStamp = Date.now();
+
+  const newMessage = new Messages({
+    message,
+    userId,
+    eventId,
+    participantsId,
+    timeStamp,
+  });
+  newMessage
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
