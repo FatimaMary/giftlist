efcaaed575369382ab95b6c4f9b6d82055ba347e
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-26 17:07:48 +0530

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,2 +1,35 @@
 import mongoose from "mongoose";
 import { autoIncrement } from "mongoose-plugin-autoinc";
+
+const messageSchema = new mongoose.Schema({
+  messageId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  Date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  likes: {
+    type: String,
+  },
+  UserId: {
+    type: String,
+    required: true,
+  },
+  eventId: {
+    type: String,
+    required: true,
+  },
+  participantsId: {
+    type: String,
+    required: true,
+  },
+});
