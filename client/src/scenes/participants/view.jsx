6fe4ec0e36089b65b32c9d557d9db55d0a9f6e7b
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-07-29 19:05:03 +0530

    Update view.jsx

--- client/src/scenes/participants/view.jsx
+++ client/src/scenes/participants/view.jsx
@@ -8,6 +8,8 @@ import {
   Card,
   CardContent,
   CardMedia,
+  DialogContentText,
+  DialogActions,
 } from "@mui/material";
 import axios from "axios";
 import Gift from "../mywishes/gift.png";
@@ -28,101 +30,81 @@ function ViewWishes({ open, onClose, participantsId, firstName, secondName }) {
   }, []);
 
   return (
-    <Dialog
-      sx={{
-        margin: "0 261px",
-        display: "flex",
-        // width: "1080px",
-        height: "500px",
-      }}
-      open={open}
-      onClose={onClose}
-    >
-      <DialogContent
-        sx={{
-          background: "#fff",
-          boxShadow: "5px 5px 25px -5px rgba(32, 32, 36, .1)",
-          borderRadius: "10px",
-          padding: "24px 20px",
-          display: "flex",
-          // flexDirection: "column",
-          width: "80%",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-          border: "1px solid #cad3dd",
-          // overflowY: "none",
-          alignItems: "center",
-        }}
+    <Box sx={{ width: "80%" }}>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        // sx={{
+        //   maxWidth: "80%",
+        // }}
       >
-        {/* <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            borderBottom: "1px solid #e8ecf1",
-            flexWrap: "wrap",
-            padding: "0 0 16px",
-            width: "100%",
-            // overflowX: "auto",
-          }}
-        > */}
-        {productDetails.map((singleDetail, i) => (
-          <Card
-            sx={{
-              width: 280,
-              height: "250px",
-              m: "1.5rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            key={i}
-          >
-            <CardContent>
-              <Box
-                sx={{
-                  height: "60%",
-                  borderBottom: "1px solid grey",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  // height="140"
-                  image={Gift}
-                  alt="Image Description"
+        <DialogTitle id="alert-dialog-title">My Wishes</DialogTitle>
+        <DialogContent>
+          {productDetails.map((singleDetail, i) => (
+            <Card
+              sx={{
+                width: 280,
+                height: "250px",
+                m: "1.5rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              key={i}
+            >
+              <CardContent>
+                <Box
                   sx={{
-                    height: "125px",
-                    width: "100px",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    // marginTop: '10px',
+                    height: "60%",
+                    borderBottom: "1px solid grey",
                   }}
-                />
-              </Box>
-              <Box
-                sx={{
-                  height: "40%",
-                  background: "#FFEAEA",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  // textAlign: "center",
-                  p: "0.5rem",
-                  "&:hover": {
-                    backgroundColor: "#C21010",
-                    color: "white",
-                    cursor: "pointer",
-                  },
-                }}
-              >
-                <Typography>{singleDetail.productUrl}</Typography>
-              </Box>
-            </CardContent>
-          </Card>
-        ))}
-        {/* </Box> */}
-      </DialogContent>
-    </Dialog>
+                >
+                  <CardMedia
+                    component="img"
+                    // height="140"
+                    image={Gift}
+                    alt="Image Description"
+                    sx={{
+                      height: "125px",
+                      width: "100px",
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                      // marginTop: '10px',
+                    }}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    height: "40%",
+                    background: "#FFEAEA",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    // textAlign: "center",
+                    p: "0.5rem",
+                    "&:hover": {
+                      backgroundColor: "#C21010",
+                      color: "white",
+                      cursor: "pointer",
+                    },
+                  }}
+                >
+                  <Typography>{singleDetail.productUrl}</Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          {/* <Button onClick={handleClose}>Disagree</Button>
+        <Button onClick={handleClose} autoFocus>
+          Agree
+        </Button> */}
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 }
 
