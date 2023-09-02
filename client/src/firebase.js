f0c83198b5b407f0e93c513c791fe9b9fa5bb518
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-09-02 17:21:22 +0530

    .env updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,23 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCkDDsWFuP44jvoSjkGfUgwnVYh9h2dxt0",
+//   authDomain: "gift-31416.firebaseapp.com",
+//   projectId: "gift-31416",
+//   storageBucket: "gift-31416.appspot.com",
+//   messagingSenderId: "836234254908",
+//   appId: "1:836234254908:web:18588938a965dc45594139",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCkDDsWFuP44jvoSjkGfUgwnVYh9h2dxt0",
-  authDomain: "gift-31416.firebaseapp.com",
-  projectId: "gift-31416",
-  storageBucket: "gift-31416.appspot.com",
-  messagingSenderId: "836234254908",
-  appId: "1:836234254908:web:18588938a965dc45594139",
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FB_APPID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENTID,
 };
 
 // Initialize Firebase
