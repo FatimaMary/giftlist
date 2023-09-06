978a7ee197f1764468c27c351211badbb3a36302
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-09-06 16:58:17 +0530

    Font corrected

--- client/src/App.js
+++ client/src/App.js
@@ -20,47 +20,52 @@ import Signup2 from "./scenes/signup2";
 import ForgotPassword from "./Components/ResetPassword";
 import { sendPasswordResetEmail } from "firebase/auth";
 import "./App.css";
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [drawStatus, setDrawStatus] = useState(false);
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: '"Poppins", sans-serif',
+    },
+  });
 
   return (
-    <BrowserRouter>
-      <MyContext.Provider
-        value={{
-          isLoggedIn,
-          setIsLoggedIn,
-          sendPasswordResetEmail,
-          // drawStatus,
-          // setDrawStatus,
-        }}
-      >
-        <DrawStatusProvider>
-          <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signup1" element={<Signup1 />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/login1" element={<Login1 />} />
-            <Route path="/signup2" element={<Signup2 />} />
-            <Route path="/reset" element={<ForgotPassword />} />
-            <Route path="/logout" element={<WelcomePage />} />
-            <Route path="/*" element={<Layout />}>
-              <Route path="eventcreate" element={<EventCreation />} />
-              <Route path="budget" element={<Budget />} />
-              <Route path="home" element={<Home />} />
-              <Route path="success" element={<Success />} />
-              <Route path="eventview" element={<EventView />} />
-              <Route path="eventview1" element={<EventView1 />} />
-              <Route path="participants" element={<Participants />} />
-              <Route path="invite" element={<Invite />} />
-              <Route path="editevent" element={<EditEvent />} />
-            </Route>
-          </Routes>
-        </DrawStatusProvider>
-      </MyContext.Provider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <MyContext.Provider
+          value={{
+            isLoggedIn,
+            setIsLoggedIn,
+            sendPasswordResetEmail,
+          }}
+        >
+          <DrawStatusProvider>
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signup1" element={<Signup1 />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/login1" element={<Login1 />} />
+              <Route path="/signup2" element={<Signup2 />} />
+              <Route path="/reset" element={<ForgotPassword />} />
+              <Route path="/logout" element={<WelcomePage />} />
+              <Route path="/*" element={<Layout />}>
+                <Route path="eventcreate" element={<EventCreation />} />
+                <Route path="budget" element={<Budget />} />
+                <Route path="home" element={<Home />} />
+                <Route path="success" element={<Success />} />
+                <Route path="eventview" element={<EventView />} />
+                <Route path="eventview1" element={<EventView1 />} />
+                <Route path="participants" element={<Participants />} />
+                <Route path="invite" element={<Invite />} />
+                <Route path="editevent" element={<EditEvent />} />
+              </Route>
+            </Routes>
+          </DrawStatusProvider>
+        </MyContext.Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
