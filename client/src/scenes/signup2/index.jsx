771b0afc3b728fe01540fd5511104ae0db69bbb9
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-09-19 12:40:51 +0530

    Update index.jsx

--- client/src/scenes/signup2/index.jsx
+++ client/src/scenes/signup2/index.jsx
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import {
-  Box,
-  Typography,
-  TextField,
-  FormControl as Form,
-  Button,
-  createTheme,
-  ThemeProvider,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import { Box, createTheme, useTheme, useMediaQuery } from "@mui/material";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db } from "../../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import axios from "axios";
 import Navbar from "../../Components/Navbar";
+import SignupPage from "../signup/SignupPage";
 
 function Signup2() {
   const [signupData, setSignupData] = useState({
@@ -25,7 +16,6 @@ function Signup2() {
   const [errors, setErrors] = useState({});
   const [dataIsCorrect, setDataIsCorrect] = useState(false);
   const navigate = useNavigate();
-  const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
   const themes = useTheme();
   const isSmallScreen = useMediaQuery(themes.breakpoints.down("sm"));
@@ -144,159 +134,15 @@ function Signup2() {
   return (
     <Box>
       <Navbar />
-      <Box
-        sx={{
-          backgroundColor: "#FFEAEA",
-          height: "85vh",
-          width: isSmallScreen ? "100%" : "100vw",
-          padding: isSmallScreen ? "20px" : "1.5rem 2.5rem",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            backgroundColor: "white",
-            border: "1px solid #C21010",
-            borderRadius: "10px",
-            width: isSmallScreen ? "100%" : "500px",
-          }}
-          color="#C21010"
-          m="1.5rem 2.5rem"
-        >
-          <Box mt={1.5} ml={2.5}>
-            <Typography variant="h5" fontWeight="bold" mb="15px">
-              Sign up for Santa Surprise
-            </Typography>
-            <Typography
-              variant="body1"
-              sx={{
-                opacity: "0.5",
-                fontSize: "1rem",
-              }}
-            >
-              It's quick and easy.
-            </Typography>
-          </Box>
-          <form onSubmit={handleSubmit}>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "10px",
-              }}
-            >
-              <Box>
-                <Typography ml="1rem" mt="1.5rem" mb="0">
-                  Email
-                </Typography>
-                <ThemeProvider theme={theme}>
-                  <TextField
-                    placeholder="Enter your Email"
-                    variant="outlined"
-                    sx={{
-                      ml: "1rem",
-                      width: isSmallScreen ? "80%" : "450px",
-                      borderRadius: "10px",
-                      "& .MuiOutlinedInput-root": {
-                        height: "40px",
-                      },
-                    }}
-                    type="email"
-                    name="email"
-                    value={signupData.email}
-                    onChange={updateHandleChange}
-                    error={!!errors.email}
-                    helperText={errors.email}
-                  />
-                </ThemeProvider>
-              </Box>
-
-              <Box>
-                <Typography ml="1rem" mt="1.5rem" mb="0">
-                  Password
-                </Typography>
-                <ThemeProvider theme={theme}>
-                  <TextField
-                    placeholder="Create Password"
-                    variant="outlined"
-                    sx={{
-                      ml: "1rem",
-                      width: "450px",
-                      borderRadius: "10px",
-                      "& .MuiOutlinedInput-root": {
-                        height: "40px",
-                      },
-                      width: isSmallScreen ? "80%" : "450px",
-                    }}
-                    type="password"
-                    name="password"
-                    value={signupData.password}
-                    onChange={updateHandleChange}
-                    error={!!errors.password}
-                    helperText={errors.password}
-                  />
-                </ThemeProvider>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                mb="15px"
-              >
-                <Button
-                  variant="contained"
-                  sx={{
-                    border: "2px solid #C21010",
-                    borderRadius: "20px",
-                    width: isSmallScreen ? "80%" : "450px",
-                    fontSize: "1rem",
-                    color: "white",
-                    background: "#C21010",
-                    textTransform: "inherit",
-                    fontWeight: "bold",
-                    "&:hover": {
-                      backgroundColor: "#fff",
-                      color: "#C21010",
-                      border: "1px solid #C21010",
-                    },
-                    marginTop: "10px",
-                  }}
-                  type="submit"
-                >
-                  Submit
-                </Button>
-                {err && <span>Something went wrong</span>}
-              </Box>
-            </Box>
-          </form>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Typography>
-              Already have an account?
-              <Button
-                sx={{
-                  textTransform: "inherit",
-                  fontSize: "1rem",
-                }}
-                onClick={handleClick}
-              >
-                Login
-              </Button>
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
+      <SignupPage
+        theme={theme}
+        handleSubmit={handleSubmit}
+        isSmallScreen={isSmallScreen}
+        signupData={signupData}
+        updateHandleChange={updateHandleChange}
+        errors={errors}
+        handleClick={handleClick}
+      />
     </Box>
   );
 }
